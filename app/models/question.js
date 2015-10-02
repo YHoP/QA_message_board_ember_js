5888d680f983d5d8d051d9d4b028f35a4d8053ad
Yvonne <ypeng.tw@gmail.com>
2015-10-02 12:18:12 -0700

    Modify the question attributes. It will match the project discription

--- app/models/question.js
+++ app/models/question.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   subject: DS.attr(),
-  username: DS.attr(),
+  author: DS.attr(),
   content: DS.attr(),
   date_added: DS.attr(),
   answers: DS.hasMany('answer', { async: true })
