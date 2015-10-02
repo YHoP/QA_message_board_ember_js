1e09992022dfd0a33cdb73f2dc42982c0b0beb6a
Yvonne <ypeng.tw@gmail.com>
2015-10-02 16:34:14 -0700

    Add answer model. It will handle the answer attribute

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,10 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  question: DS.belongsTo('question', { async: true }),
   author: DS.attr(),
-  subject: DS.attr(),
   content: DS.attr(),
   date_added: DS.attr(),
-  date_edited: DS.attr(),
-  answers: DS.hasMany('answer', { async: true })
+  date_edited: DS.attr()
 });
